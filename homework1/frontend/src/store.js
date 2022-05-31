80214cf5e804f482d18ed84cbe261127c1e3c5b1
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-31 10:50:39 +0700

    Create Store

---
+++ homework1/frontend/src/store.js
@@ -0,0 +1,11 @@
+import { createStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+    reducer : {
+
+
+    }
+})
+
+export default store;
