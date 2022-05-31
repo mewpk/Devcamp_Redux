85b4d9e96c88f7376c9dee193111d09748f44cc5
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-31 11:01:14 +0700

    Insert Reducer in store

--- homework1/frontend/src/store.js
+++ homework1/frontend/src/store.js
@@ -1,9 +1,10 @@
-import { createStore } from "@reduxjs/toolkit";
+import { createStore} from 'redux'
 import { configureStore } from "@reduxjs/toolkit";
+import profileReducer from "./Reducers/profileReducer";
 
 const store = configureStore({
     reducer : {
-
+        profile : profileReducer
 
     }
 })
