5eb1ea7a736829ab471fe8c7d495e1721c829961
KaWaIINa <81364308+millove123@users.noreply.github.com>
2022-05-31 11:58:52 +0700

    save

--- homework1/frontend/src/Components/Profile.jsx
+++ homework1/frontend/src/Components/Profile.jsx
@@ -1,7 +1,73 @@
-import React from 'react'
+import React from "react";
+import "antd/dist/antd.css";
+import { Table, Tag, Space } from "antd";
+const { Column, ColumnGroup } = Table;
+import { useSelector , useDispath } from "react-redux";
+import {insertData} from "../Reducers/profileReducer"
 
 export default function Profile() {
+  //get value state
+
+  const data = [
+    {
+      key: "1",
+      firstName: "John",
+      lastName: "Brown",
+      age: 32,
+      address: "New York No. 1 Lake Park",
+      tags: ["nice", "developer"],
+    },
+    {
+      key: "2",
+      firstName: "Jim",
+      lastName: "Green",
+      age: 42,
+      address: "London No. 1 Lake Park",
+      tags: ["loser"],
+    },
+    {
+      key: "3",
+      firstName: "Joe",
+      lastName: "Black",
+      age: 32,
+      address: "Sidney No. 1 Lake Park",
+      tags: ["cool", "teacher"],
+    },
+  ];
   return (
-    <div>Profile</div>
-  )
+    <div style={{ height: "100vh" , border : "3px solid black" }}>
+      <Table dataSource={data}>
+        <ColumnGroup title="Name">
+          <Column title="First Name" dataIndex="firstName" key="firstName" />
+          <Column title="Last Name" dataIndex="lastName" key="lastName" />
+        </ColumnGroup>
+        <Column title="Age" dataIndex="age" key="age" />
+        <Column title="Address" dataIndex="address" key="address" />
+        <Column
+          title="Tags"
+          dataIndex="tags"
+          key="tags"
+          render={(tags) => (
+            <>
+              {tags.map((tag) => (
+                <Tag color="blue" key={tag}>
+                  {tag}
+                </Tag>
+              ))}
+            </>
+          )}
+        />
+        <Column
+          title="Action"
+          key="action"
+          render={(_, record) => (
+            <Space size="middle">
+              <a>Invite {record.lastName}</a>
+              <a>Delete</a>
+            </Space>
+          )}
+        />
+      </Table>
+    </div>
+  );
 }
